20398d9f0cc1b8a752891a101892cab817fd39e4
Karishnu Poddar <karishnu@gmail.com>
2016-10-15 10:12:21 +0530

    data save functions created

--- app/src/main/java/com/ieeecsvit/riviera17android/rest/ApiInterface.java
+++ app/src/main/java/com/ieeecsvit/riviera17android/rest/ApiInterface.java
@@ -1,7 +1,14 @@
 package com.ieeecsvit.riviera17android.rest;
 
+import com.ieeecsvit.riviera17android.models.Events;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface ApiInterface {
     //@POST("auth")
     //Call<LoginVerify> login(@Body LoginRequest loginRequest);
+    @GET("events/all")
+    Call<Events> events();
 
 }
---
+++ app/src/main/java/com/ieeecsvit/riviera17android/rest/Data.java
@@ -0,0 +1,41 @@
+package com.ieeecsvit.riviera17android.rest;
+
+import android.app.Activity;
+
+import com.ieeecsvit.riviera17android.RealmController;
+import com.ieeecsvit.riviera17android.models.Event;
+import com.ieeecsvit.riviera17android.models.Events;
+
+import io.realm.Realm;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * Created by Karishnu Poddar on 15/10/2016.
+ */
+public class Data {
+    public static void updateEvents(final Activity activity){
+
+        ApiInterface apiInterface = new ApiClient().getClient(activity).create(ApiInterface.class);
+        Call<Events> getEvents = apiInterface.events();
+
+        getEvents.enqueue(new Callback<Events>() {
+            @Override
+            public void onResponse(Call<Events> call, Response<Events> response) {
+                Realm realm = RealmController.with(activity).getRealm();
+
+                for (Event e : response.body().events) {
+                    realm.beginTransaction();
+                    realm.copyToRealm(e);
+                    realm.commitTransaction();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Events> call, Throwable t) {
+
+            }
+        });
+    }
+}
