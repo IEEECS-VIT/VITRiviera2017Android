7537c3c82fae520cd797b17f34bf08b3f4cd9d2a
Mayank Aggarwal <mayankagg9722@gmail.com>
2016-10-18 22:21:15 +0530

    main activity

--- app/src/main/java/com/ieeecsvit/riviera17android/MainActivity.java
+++ app/src/main/java/com/ieeecsvit/riviera17android/MainActivity.java
@@ -1,8 +1,11 @@
 package com.ieeecsvit.riviera17android;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,17 +15,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    TextView pre,work,formal,informal,cyber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setBackgroundColor(Color.parseColor("#302236"));
 
+        getSupportActionBar().setTitle("Riviera '17");
+
+        pre=(TextView)findViewById(R.id.pretext);
+        work=(TextView)findViewById(R.id.workshoptext);
+        formal=(TextView)findViewById(R.id.formaltext);
+        informal=(TextView)findViewById(R.id.informaltext);
+        cyber=(TextView)findViewById(R.id.cybertext);
+
+        Typeface typeface=Typeface.createFromAsset(getAssets(), "fonts/Montserrat-Regular.ttf");
+
+        pre.setTypeface(typeface);
+        work.setTypeface(typeface);
+        formal.setTypeface(typeface);
+        informal.setTypeface(typeface);
+        cyber.setTypeface(typeface);
+
+        
+
+        /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +59,7 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
+*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -98,4 +127,27 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    public void preriviera(View view){
+        Toast.makeText(this, "pre", Toast.LENGTH_SHORT).show();
+    }
+
+    public void workshop(View view){
+        Toast.makeText(this, "work", Toast.LENGTH_SHORT).show();
+
+    }
+    public void formal(View view){
+        Toast.makeText(this, "formal", Toast.LENGTH_SHORT).show();
+
+    }
+    public void informal(View view){
+        Toast.makeText(this, "informal", Toast.LENGTH_SHORT).show();
+
+    }
+    public void cyber(View view){
+        Toast.makeText(this, "cyber", Toast.LENGTH_SHORT).show();
+
+    }
+
+
 }
