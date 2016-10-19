14d49360334d18820e53bd325b3b5cdb4d21a0b4
Mayank Aggarwal <mayankagg9722@gmail.com>
2016-10-19 22:56:42 +0530

    nav and login anim

--- app/src/main/java/com/ieeecsvit/riviera17android/LoginActivity.java
+++ app/src/main/java/com/ieeecsvit/riviera17android/LoginActivity.java
@@ -2,20 +2,48 @@ package com.ieeecsvit.riviera17android;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.dynamitechetan.flowinggradient.FlowingGradient;
+import com.dynamitechetan.flowinggradient.FlowingGradientClass;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button login,viewevent;
     EditText regno,pass;
+    ImageView background;
+    RelativeLayout relativeLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        relativeLayout=(RelativeLayout)findViewById(R.id.activity_login);
+        FlowingGradientClass grad=new FlowingGradientClass();
+        grad.setBackgroundResource(R.drawable.gradient)
+        .onRelativeLayout(relativeLayout).
+        setTransitionDuration(2500).start();
+
+/*
+        background=(ImageView)findViewById(R.id.backgroundimage);
+        background.setBackgroundResource(R.drawable.gradient);
+
+        Animation fadeanim=AnimationUtils.loadAnimation(this,R.anim.fadein);
+        background.startAnimation(fadeanim);
+
+        AnimationDrawable changeBack=(AnimationDrawable)background.getBackground();
+        changeBack.start();
+*/
 
         login=(Button)findViewById(R.id.loginbutton);
         Typeface typeface=Typeface.createFromAsset(getAssets(), "fonts/orator.ttf");
