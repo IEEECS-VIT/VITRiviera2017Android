50f2e0375c9556ed6b81d8e19cca87c3435a0379
mayankagg9722 <mayankagg9722@gmail.com>
2016-11-29 01:02:44 +0530

    Wishlist Layout Changed

--- app/src/main/java/com/ieeecsvit/riviera17android/activity/WishlistActivity.java
+++ app/src/main/java/com/ieeecsvit/riviera17android/activity/WishlistActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.ieeecsvit.riviera17android.R;
 import com.ieeecsvit.riviera17android.RVEventListAdapter;
@@ -11,22 +14,42 @@ import com.ieeecsvit.riviera17android.RealmController;
 import com.ieeecsvit.riviera17android.WishlistAdapter;
 import com.ieeecsvit.riviera17android.models.Event;
 
+import org.w3c.dom.Text;
+
 public class WishlistActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
-    private WishlistAdapter wishlistAdapter;
+    private WishlistAdapter wishlistAdapter,item_countAdapter;
+    TextView wish_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_wishlist);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        setContentView(R.layout.activity_wishlist);
+        getSupportActionBar().setTitle("Wishlist");
+
+        wish_text=(TextView)findViewById(R.id.wish_text);
+        wish_text.setText("");
+
+        ;
         recyclerView = (RecyclerView) findViewById(R.id.rv_wishlist);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         wishlistAdapter= new WishlistAdapter(RealmController.with(this).getEvents(),this);
-        recyclerView.setAdapter(new RVEventListAdapter(RealmController.with(this).getRealm().where(Event.class).equalTo("checked",true).findAll(),this,true));
+
+        item_countAdapter=new WishlistAdapter(RealmController.with(this).getRealm().where(Event.class).equalTo("checked",true).findAll(),this);
+
+        Log.v("wish",String.valueOf(item_countAdapter.getItemCount()));
+
+        if(item_countAdapter.getItemCount()==0){
+            recyclerView.setAdapter(new RVEventListAdapter(RealmController.with(this).getRealm().where(Event.class).equalTo("checked",true).findAll(),this,true));
+            wish_text.setText("No Events Added To Wishlist!");
+        }
+        else if(item_countAdapter.getItemCount()>0){
+            recyclerView.setAdapter(new RVEventListAdapter(RealmController.with(this).getRealm().where(Event.class).equalTo("checked",true).findAll(),this,true));
+            wish_text.setText("");
+        }
 
     }
 }
