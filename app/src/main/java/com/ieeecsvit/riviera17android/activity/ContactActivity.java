d39f8009bb96bf2288990a85990af3391f532630
AKANSHI SRIVASTAVA <AKANSHI SRIVASTAVA>
2016-12-02 21:40:01 +0530

    Contactpage

--- app/src/main/java/com/ieeecsvit/riviera17android/activity/ContactActivity.java
+++ app/src/main/java/com/ieeecsvit/riviera17android/activity/ContactActivity.java
@@ -1,15 +1,34 @@
 package com.ieeecsvit.riviera17android.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.ieeecsvit.riviera17android.R;
 
 public class ContactActivity extends AppCompatActivity {
 
+    TextView textView1;
+    TextView textView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+        textView1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                String phone_no = textView1.getText().toString().replaceAll("-", "");
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse(phone_no));
+                callIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(callIntent);
+            }
+        });
     }
 }
